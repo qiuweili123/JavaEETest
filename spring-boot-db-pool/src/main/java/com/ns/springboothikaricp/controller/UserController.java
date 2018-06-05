5c83bc1e296bb18b79bb781f75f021bd518bb65a
liqiuwei <qiuweili123@163.com>
2018-06-05 22:51:47 +0800

    add  swagger

--- spring-boot-db-pool/src/main/java/com/ns/springboothikaricp/controller/UserController.java
+++ spring-boot-db-pool/src/main/java/com/ns/springboothikaricp/controller/UserController.java
@@ -1,12 +1,15 @@
 package com.ns.springboothikaricp.controller;
 
+import com.ns.springboothikaricp.bean.User;
 import com.ns.springboothikaricp.dao.UserInfoMapper;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 
+@Api(value = "/test", tags = "测试接口模块")
 @RestController
 @RequestMapping("/user")
 public class UserController {
@@ -22,5 +25,12 @@ public class UserController {
 
             return userInfoMapper.selectByOddUserId(id);
         }
+    }
+
+    @ApiOperation(value = "添加用户信息", notes = "添加用户信息")
+    @ApiImplicitParam(name = "user", value = "User", required = true, dataType = "User")
+    @PostMapping("/addUser")
+    public Object addUser(@RequestBody User user) {
+        return "success";
     }
 }
