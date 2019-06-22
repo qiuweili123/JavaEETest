ff97200adce6c1c59db1c18b58a84c327debcd49
qiuweili123 <qiuweili123@163.com>
2019-06-22 17:23:44 +0800

    modify

--- openresty-arch-demo/src/main/lua/image/rewirte_url.lua
+++ openresty-arch-demo/src/main/lua/image/rewirte_url.lua
@@ -2,14 +2,14 @@ local ngx_var = ngx.var
 local ngx_log = ngx.log
 local debug = ngx.DEBUG
 
-function parseUri(uri)
+local function parseUri(uri)
 
     local _, _, name, ext, size = string.find(uri, "(.+)(%..+)!(%d+x%d+)")
 
     if name and size and ext then
         return ngx.var.image_root .. name .. ext, size
     else
-        return "",""
+        return "", ""
     end
 
 end
@@ -18,14 +18,21 @@ local function resize()
     local ori_filename, size = parseUri(ngx_var.uri)
     -- ngx_log(debug,);
     --ngx.print(ori_filename)
-   -- ngx_log(debug,ori_filename)
+    -- ngx_log(debug,ori_filename)
+
+    -- print_data("0","sdsdsd");
 end
 
+local function print_data(code, data)
+    local responseBody = ngx.location.capture("/httpStatusRewrite", { method = ngx.Http_POST, body = "测试", args = { apiRespCodeParam = code } });
 
-  function test()
-    ngx.print(package.path)
-    -- ngx.say("ytest rerite" .. ngx_var.project_name);
+    ngx.exit(ngx.HTTP_OK)
 end
 
-
-test()
+local responseBody = ngx.location.capture("/httpStatusRewrite", { method = ngx.Http_POST, body = "测试", args = { apiRespCodeParam = 0 } });
+ngx.print(responseBody.body);
+ngx.exit(ngx.HTTP_OK);
+-- ngx_log(debug,"this is responseBody :",responseBody.body);
+-- ngx_log.log(debug,"package.path",package.path);
+--- test()
+-- resize();
